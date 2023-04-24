b9b8243bd25cba5669d168a50455bd75cffbc159
JazminVa <yasminvargasr05@gmail.com>
2023-04-24 16:47:08 -0500

    pedido enviado

--- client/src/pages/Pedidos.jsx
+++ client/src/pages/Pedidos.jsx
@@ -1,5 +1,49 @@
-function Pedidos() {
-    return (<p>Pedidos</p>)
-}
 
-export default Pedidos
+function Pedidos() {
+    const [nombre, setNombre] = useState("");
+    const [direccion, setDireccion] = useState("");
+    const [producto, setProducto] = useState("");
+    const [cantidad, setCantidad] = useState(0);
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("Nombre:", nombre);
+      console.log("Dirección:", direccion);
+      console.log("Producto:", producto);
+      console.log("Cantidad:", cantidad);
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nombre:
+          <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Dirección:
+          <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Producto:
+          <select value={producto} onChange={(e) => setProducto(e.target.value)}>
+            <option value="">Seleccione un producto</option>
+            <option value="Pan">Pan</option>
+            <option value="Doritos">Doritos</option>
+            <option value="Perro caliente">Perro caliente</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Cantidad:
+          <input type="number" value={cantidad} onChange={(e) => setCantidad(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Enviar pedido</button>
+      </form>
+    );
+  }
+
+  
+export default Pedidos 
